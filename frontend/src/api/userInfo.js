e4f94691c25376586cdeef0421505a302f719f79
kadlub <kuba.kadlubowski@gmail.com>
2024-12-22 12:22:26 +0100

    Login and registration working

--- frontend/src/api/userInfo.js
+++ frontend/src/api/userInfo.js
@@ -1,73 +1,73 @@
 import axios from "axios";
 import { API_BASE_URL, getHeaders } from "./constant";
 
-export const fetchUserDetails = async ()=>{
+export const fetchUserDetails = async () => {
     const url = API_BASE_URL + '/api/user/profile';
-    try{
-        const response = await axios(url,{
-            method:"GET",
-            headers:getHeaders()
+    try {
+        const response = await axios(url, {
+            method: "GET",
+            headers: getHeaders()
         });
         return response?.data;
     }
-    catch(err){
+    catch (err) {
         throw new Error(err);
     }
 }
 
-export const addAddressAPI = async (data)=>{
+export const addAddressAPI = async (data) => {
     const url = API_BASE_URL + '/api/address';
-    try{
-        const response = await axios(url,{
-            method:"POST",
-            data:data,
-            headers:getHeaders()
+    try {
+        const response = await axios(url, {
+            method: "POST",
+            data: data,
+            headers: getHeaders()
         });
         return response?.data;
     }
-    catch(err){
+    catch (err) {
         throw new Error(err);
     }
 }
 
-export const deleteAddressAPI = async (id)=>{
+export const deleteAddressAPI = async (id) => {
     const url = API_BASE_URL + `/api/address/${id}`;
-    try{
-        const response = await axios(url,{
-            method:"DELETE",
-            headers:getHeaders()
+    try {
+        const response = await axios(url, {
+            method: "DELETE",
+            headers: getHeaders()
         });
         return response?.data;
     }
-    catch(err){
+    catch (err) {
         throw new Error(err);
     }
 }
 
-export const fetchOrderAPI = async ()=>{
+export const fetchOrderAPI = async () => {
     const url = API_BASE_URL + `/api/order/user`;
-    try{
-        const response = await axios(url,{
-            method:"GET",
-            headers:getHeaders()
+    try {
+        const response = await axios(url, {
+            method: "GET",
+            headers: getHeaders()
         });
         return response?.data;
     }
-    catch(err){
+    catch (err) {
         throw new Error(err);
     }
 }
 
-export const cancelOrderAPI = async (id)=>{
+export const cancelOrderAPI = async (id) => {
     const url = API_BASE_URL + `/api/order/cancel/${id}`;
-    try{
-        const response = await axios(url,{
-            method:"POST",
-            headers:getHeaders()
+    try {
+        const response = await axios(url, {
+            method: "POST",
+            headers: getHeaders()
         });
         return response?.data;
     }
-    catch(err){
+    catch (err) {
         throw new Error(err);
     }
 }
